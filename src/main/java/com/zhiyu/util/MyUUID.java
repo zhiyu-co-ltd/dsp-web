51349e8f46191a22d363c17ddadc0ab202867c38
zhaojianfan <zjf198100@163.com>
2017-03-05 17:17:22 +0800

    增加其他页面访问控制

---
+++ src/main/java/com/zhiyu/util/MyUUID.java
@@ -0,0 +1,21 @@
+package com.zhiyu.util;
+
+import java.util.UUID;
+
+/**
+ * Created by zhaojianfan on 17/1/19.
+ */
+public class MyUUID {
+
+    public MyUUID(){
+
+    }
+
+    public static String getUUID() {
+        UUID uuid = UUID.randomUUID();
+        String str = uuid.toString();
+        // 去掉"-"符号
+        String temp = str.substring(0, 8) + str.substring(9, 13) + str.substring(14, 18) + str.substring(19, 23) + str.substring(24);
+        return temp;
+    }
+}
